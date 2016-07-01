e75f537bb842ef2b48e4989e979abd43ee851e64
chenfei7 <chenfei7@bj-m-203687a.local>
2016-07-01 19:15:50 +0800

    first commit

---
+++ multiboot_header.asm
@@ -0,0 +1,17 @@
+header_start:
+	dd 0xe85250d6 ;magic number
+	dd 0		  ;protected mode code
+	dd header_start - header_end; header length
+	
+	;checksum
+	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+	;required end tag
+	dw 0 ;type
+	dw 0 ;flags
+	dd 8 ;size
+header_end:
+
+
+
+

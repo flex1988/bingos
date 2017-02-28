fb2a7dfe96a1cd0ccf227d3725df99ce77350c04
chenfei7 <chenfei7@staff.weibo.com>
2017-02-28 23:33:31 +0800

    print to screen

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,26 @@
 #![feature(lang_items)]
+#![feature(unique)]
+#![feature(const_fn)]
 #![no_std]
-
 extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let hello = b"hello world!";
-    let color_byte = 0x1f;
-
-    let mut hello_colored = [color_byte;24];
-    for(i,char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000+1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
     loop{}
 }
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
-
-
+#[lang = "eh_personality"] 
+extern fn eh_personality() {}
+#[lang = "panic_fmt"] 
+#[no_mangle] 
+pub extern fn panic_fmt() -> ! {loop{}}
 
 #[allow(non_snake_case)]
 #[no_mangle]
